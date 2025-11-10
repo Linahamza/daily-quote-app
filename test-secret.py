b3f449a6cfb22bbb84123e9aea65f9ef464832d0
Linahamza <lina94.hamza@gmail.com>
2025-11-11 00:06:40 +0100

    Remove test secrets

--- test-secret.py
+++
@@ -1,4 +0,0 @@
-# test-secret.py
-# ATTENTION : Fichier de test uniquement (à supprimer après)
-API_KEY = "AKIAIOSFODNN7EXAMPLE0abcdefghijklmnopqrstuvwxyz"  # Secret détectable
-DATABASE_URL = "postgresql://admin:password123@localhost/db"
