d22c5a1b49f088c9de24b3084751ae34794bd6ca
Linahamza <lina94.hamza@gmail.com>
2025-11-10 23:26:58 +0100

    Remove test secrets file

--- test-secret.py
+++
@@ -1,5 +0,0 @@
-# test-secret.py
-# ATTENTION : Fichier de test uniquement (à supprimer après)
-GITHUB_TOKEN = "ghp_abcdefghijklmnopqrstuvwxyz123456789012"  
-OPENAI_KEY = "sk-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456" 
-
